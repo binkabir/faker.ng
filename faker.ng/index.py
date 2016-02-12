da2a975201fa41dbea0f8121c927cf311427f356
Shuaib Afebua <asekome.afegbua@gmail.com>
2016-02-12 10:29:12 +0100

    Implemented route for people/<numOfPeople>

--- faker.ng/index.py
+++ faker.ng/index.py
@@ -6,9 +6,16 @@ app = Flask(__name__)
 
 
 @app.route('/api/v1/faker/people')
-def get_fake_people():    
+def get_fake_people():
 
     dat = DB().find_people()
+    resp = Response(json.dumps(dat), status=200, mimetype="application/json")
+    return resp
+
+@app.route('/api/v1/faker/people/<int:numOfPeople>')
+def get_no_of_people(numOfPeople):
+
+    dat = DB().find_people()[0:numOfPeople]
     resp = Response(json.dumps(dat), status=200, mimetype="application/json")
     return resp
 
