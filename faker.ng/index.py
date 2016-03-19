a056b2316f9e422b385ddb7a5c34805ccb010b81
Muhammad Kabiru Idris <midris@swglobal.com>
2016-03-19 22:16:43 +0100

    added the api_version in config file

--- faker.ng/index.py
+++ faker.ng/index.py
@@ -6,6 +6,10 @@ from flask import Response
 
 app = Flask(__name__)
 app.config.from_pyfile("application.cfg") #added the configuration params file
+
+api = app.config["API_VERSION"]
+
+
 PRODUCTION = False
 if PRODUCTION:
     PROJECT_PATH = os.path.split(os.path.abspath((__file__)))[0] +  os.path.sep
@@ -24,22 +28,22 @@ def home():
 #    return resp
 
 
-@app.route('/api/v1/faker/people/')
-@app.route('/api/v1/faker/people/<int:numOfPeople>')
+@app.route(api + '/people/')
+@app.route(api + '/people/<int:numOfPeople>')
 def get_number_of_people(numOfPeople = app.config["DEFAULT_QUERY_SIZE"]):
  
     dat = DB().find_people()[:numOfPeople]
     resp = Response(json.dumps(dat), status=200, mimetype="application/json")
     return jsonify(size=len(dat), response = dat)
 
-@app.route('/api/v1/faker/emails')
+@app.route(api + '/emails')
 def get_emails():
 
     data = DB().find_emails()
     resp = Response(json.dumps(data), status=200, mimetype="application/json")
     return resp
-
-@app.route('/api/v1/faker/emails/<int:numOfEmails>')
+ 
+@app.route(api +  '/emails/<int:numOfEmails>')
 def get_num_of_emails(numOfEmails):
 
     data = DB().find_emails()[:numOfEmails]
